e3bc331c9ee256b0a9de99afa21ec7abf9abee87
Adrian Busin <busi@zhaw.ch>
2015-01-15 15:20:43 +0100

    second commit
    Signed-off-by: Adrian Busin <busi@zhaw.ch>

---
+++ ZHAW LSFM Exam AppTests/ZHAW_LSFM_Exam_AppTests.m
@@ -0,0 +1,40 @@
+//
+//  ZHAW_LSFM_Exam_AppTests.m
+//  ZHAW LSFM Exam AppTests
+//
+//  Created by Adrian on 10.12.14.
+//  Copyright (c) 2014 Adrian Busin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ZHAW_LSFM_Exam_AppTests : XCTestCase
+
+@end
+
+@implementation ZHAW_LSFM_Exam_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
